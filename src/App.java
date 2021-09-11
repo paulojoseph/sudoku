076b703f176a8e2d3dc82f7f07c8bed92e219391
Yuri Ageme <yuriageme@gmail.com>
2021-09-11 17:51:09 -0300

    Método Resolver
    Inclusão doa métodos checarLinha(), checarColuna(),ChecarQuadrante e Resolver()

--- src/App.java
+++ src/App.java
@@ -2,7 +2,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         Sudoku sudoku = new Sudoku();
 
+        System.out.println("Novo Tabuleiro: \n");
+
         sudoku.novoTabuleiro(Sudoku.matrizJogo);
+
+        sudoku.exibir();
+
+        sudoku.resolver();
+
+        System.out.println("Tabuleiro Resolvido: \n");
 
         sudoku.exibir();
 
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -47,7 +47,71 @@ public class Sudoku {
             
             System.out.println();
         }
-      
-      // ...
+
+        // Verifica se o valor inserido já foi usado na mesma linha
+        private boolean checarLinha(int lin, int valor) {
+            for (int i = 0; i < tamanho; i++)
+                if (tabuleiro[lin][i] == valor){
+                    return true;
+                }
+            return false;
+        }    
+
+        // Verifica se o valor inserido já foi usado na mesma coluna
+        private boolean checarColuna(int col, int valor) {
+            for (int i = 0; i < tamanho; i++)
+                if (tabuleiro[i][col] == valor){
+                    return true;
+                }
+            return false;
+        }
+
+        // Verifica se o valor inserido já foi usado no mesmo quadrante
+        private boolean checarQuadrante(int lin, int col, int valor) {             
+            int l = lin - lin % 3; //de 0 a 2 retorna 0, de 3 a 5 retorna 3 e de 6 a 8 retorna 6.
+            int c = col - col % 3;
+            // Os números 0, 3 e 6 representam o inicio de cada quadrante.
+            // O for irá percorrer todas as casas do quadrande de [l][c] a [l+3][c+3]
+            for (int i = l; i < l + 3; i++)
+                for (int j = c; j < c + 3; j++)
+                    if (tabuleiro[i][j] == valor){
+                        return true;
+                    }
+            return false;
+        }
+
+        // Combina os métodos de validação de linha, coluna e quadrante para validar o valor inserido em uma posição
+        private boolean validarPosicao (int lin, int col, int valor) {
+		    return !checarLinha(lin, valor)  &&  !checarColuna(col, valor)  &&  !checarQuadrante(lin, col, valor);
+    	}
+
+        // Método para resolver o sudoku utilizando algoritmo de Backtraking.
+        public boolean resolver() {
+            for (int lin = 0; lin < tamanho; lin++) {
+                for (int col = 0; col < tamanho; col++) {
+                    // Busca por células vazias (0)
+                    if (tabuleiro[lin][col] == vazio) {
+                        // Tenta valores possiveis para a posição
+                        for (int valor = 1; valor <= tamanho; valor++) {
+                            if (validarPosicao(lin, col, valor)) {
+                                // Valor corresponde as regras do sudoku
+                                tabuleiro[lin][col] = valor;
+
+                                if (resolver()) { // Faz o Backtraking chmando a função resolver recursivamente
+                                    return true;
+                                } else { // Se solução inválida, limpa a célula e continua
+                                    tabuleiro[lin][col] = vazio;
+                                }
+                            }
+                        }
+
+                        return false;
+                    }
+                }
+            }
+
+            return true; // sudoku resolvido
+	    }
+
     }
     
