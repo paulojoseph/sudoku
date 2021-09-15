18801bd14a6d838f1819d10a0133e438dd586e5a
Paulo Marques <79942696+paulojoseph@users.noreply.github.com>
2021-09-15 19:12:02 -0300

    Atualização App.Java
    Atualização de Cores do cabeçalho

--- src/App.java
+++ src/App.java
@@ -1,13 +1,61 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Sudoku sudoku = new Sudoku();
+        Scanner sc = new Scanner(System.in);
 
+        //Scanner sc = new Scanner(System.in);
+        int nivelDificuldade = -1;
+        boolean loopPergunta = true;
+        String reset = "\u001B[0;0;0m";     //define cores
+        String msgFacil = "\033[42m"; //define cores
+        String msgMedio = "\033[43m"; //define cores
+        String msgDificil = "\033[41m"; //define cores
+        String msgSistema = "\033[1;1;105m"; //define cores
+        String msgSistema2 = "\033[1;90m"; //define cores
+
+        Sudoku sudoku = new Sudoku();
         Jogador jogador = new Jogador();
 
-        System.out.println("Novo Tabuleiro: \n");
+        System.out.println(msgSistema + "+++++++++++++++++++++++++++++++++++++++");
+        System.out.println("++++++++++++++ " + msgSistema2 + "SUDOKU +++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++" + reset);
+        System.out.println("|   Escolha uma dificuldade abaixo:   |");
+        System.out.println("|   1 - " + msgFacil + "Fácil" + reset + "                         |");
+        System.out.println("|   2 - " + msgMedio + "Média" + reset + "                         |");
+        System.out.println("|   3 - " + msgDificil + "Difícil" + reset + "                         |");
+        System.out.println("---------------------------------------");
+        
+        do {
+            System.out.print("Digite o numero da opção desejada:\n> ");
+            nivelDificuldade = Integer.parseInt(sc.next());
+            if (nivelDificuldade < 1 || nivelDificuldade > 3)
+                System.out.println(msgSistema + "Opção inválida! Escolha 1, 2 ou 3" + reset);
+            else
+                loopPergunta = false;
+        } while (loopPergunta);
 
-        sudoku.criarNovoTabuleiro(Sudoku.matrizJogo);
+        
+        if (nivelDificuldade == 1) {
+
+            System.out.print("\n     " + msgFacil + " Dificuldade: Fácil  " + reset);
+            sudoku.criarNovoTabuleiro(Sudoku.matrizFacil);  
+        }
+        if (nivelDificuldade == 2) {
+
+            System.out.print("\n     " + msgMedio + " Dificuldade: Média  " + reset);
+            sudoku.criarNovoTabuleiro(Sudoku.matrizMedio);            
+        }
+        if (nivelDificuldade == 3) {
+
+            System.out.print("\n     " + msgDificil + " Dificuldade: Difícil" + reset);
+            sudoku.criarNovoTabuleiro(Sudoku.matrizDificil);            
+        }     
+
+        System.out.println("\n\n\033[1;120;20mTabuleiro:\033[0m\n");
+
+        //sudoku.criarNovoTabuleiro(Sudoku.matrizFacil);
 
         sudoku.exibir();
 
@@ -16,20 +64,27 @@ public class App {
             jogador.entrarDados();
 
             while(!sudoku.checarCelulaVazia(jogador.lin, jogador.col) || (jogador.valor <= 0) || (jogador.valor > 9)){
-                System.out.println("Jogada inválida. Usar números de 1 a 9 para selecionar células vazias (0)");
+                System.out.println("Jogada inválida. Usar números de 1 a 9 para selecionar células vazias (0)\n");
+                sudoku.exibir();
                 jogador.entrarDados();
+                sudoku.exibir();
             }
+
             sudoku.criarCopiaTabuleiro();
+            
             if (sudoku.validarPosicao(jogador.lin, jogador.col, jogador.valor)){
                 sudoku.inserir(jogador.lin, jogador.col, jogador.valor);
+                sudoku.criarCopiaTabuleiro();
                 if (!sudoku.resolver()){
                     sudoku.remover(jogador.lin, jogador.col, jogador.valor);
-                    System.out.println("Jogada inválida: Tabuleiro sem resolução, tente outra vez!");
+                    System.out.println("Jogada inválida: Tabuleiro sem resolução, tente outra vez!\n");
                 }else{
                     sudoku.exibir();
                 }
             }else{
-                System.out.println("Jogada inválida: Número repetido, tente outra vez!");
+                System.out.println("Jogada inválida: Número repetido, tente outra vez!\n");
+                //sudoku.remover(jogador.lin, jogador.col, jogador.valor);
+                sudoku.exibir();
             }
         
         }
